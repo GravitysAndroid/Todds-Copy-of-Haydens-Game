5e940f6a912f687db8f632ee2ef5bc02e5088f74
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2020-11-20 10:59:43 +1300

    Last minute changes

---
+++ Assets/Scripts/Controller/ShakeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeScript : MonoBehaviour
+{
+	// References to controlled game objects that will fall
+	// when you shake the mobile device hard enough
+	public GameObject PanelOne, PanelTwo;
+
+	// variable to hold shaking acceleration value
+	Vector3 accelerationDir;
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Read new acceleration Input from mobile device
+		accelerationDir = Input.acceleration;
+
+		// If you shake the mobile device hard enough
+		// (accelerations Square Magnitude greater then 5 for example)
+		if (accelerationDir.sqrMagnitude >= 5f)
+		{
+			PanelOne.SetActive(true);
+			PanelTwo.SetActive(false);
+		}
+	}
+}
